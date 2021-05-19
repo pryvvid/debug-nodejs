ba61b43eb9a96ab8f26a22ce64f1b36940706c29
pryvvid <pryvvid@gmail.com>
2021-05-19 10:37:33 +0300

    fix: app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var db = require('./db');
@@ -6,10 +7,10 @@ var game = require('./controllers/gamecontroller')
 
 
 db.sync();
-app.use(require('body-parser'));
+app.use(express.json());
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
-app.listen(function() {
+app.listen(4000, function() {
     console.log("App is listening on 4000");
 })
